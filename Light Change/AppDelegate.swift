37c5c5e49bf26727744fa56f3da9e494a573aae4
Lakshay Bhatia <lakshaybhatia999@gmail.com>
2021-02-10 19:18:39 +1100

    Initial Commit

---
+++ Light Change/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Light Change
+//
+//  Created by Lakshay Bhatia on 10/2/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
